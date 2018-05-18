68853c97f9f5fdcd2fbc6697c1391ca523353aba
matthiasdurivet <Matthias.durivet@student.howest.be>
2018-05-18 14:45:06 +0200

    bug fixing manifest

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -8,6 +8,33 @@ const buttonToScreen = {'katakana': katakana, 'stats': stats, 'flashcard': flash
 
 alert("v7");
 
+let installPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    installPrompt = e;
+    installPrompt.prompt();
+});
+
+/*installPrompt.addEventListener('click', (e) => {
+ // hide our user interface that shows our A2HS button
+ installPrompt.style.display = 'none';
+ // Show the prompt
+ installPrompt.prompt();
+ // Wait for the user to respond to the prompt
+ installPrompt.userChoice
+ .then((choiceResult) => {
+ if (choiceResult.outcome === 'accepted') {
+ console.log('User accepted the A2HS prompt');
+ } else {
+ console.log('User dismissed the A2HS prompt');
+ }
+ installPrompt = null;
+ });
+ });*/
+
 document.addEventListener("DOMContentLoaded", function () {
 
     if ('serviceWorker' in navigator) {
