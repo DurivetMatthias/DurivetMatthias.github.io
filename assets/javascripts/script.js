62ec305247f9c9ce278dc619f6aa68b1151413ac
DurivetMatthias <matthias.durivet@student.howest.be>
2019-06-06 15:14:05 +0200

    add rotation

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,16 +1,29 @@
 Vue.component('life-counter', {
     data: function () {
         return {
-            count: 20
+            count: 20,
+            rotation: 0,
+        }
+    },
+    computed:{
+        rstyle () {
+            return 'transform: rotate(' + this.rotation + 'deg)'
         }
     },
     template: `
-        <div class='life-counter'>
+        <div class='life-counter' :style="rstyle">
+            {{rstyle}}
             <button class='add' v-on:click='count++'>+</button>
-            <p>{{ count }}</p>
             <button class='sub' v-on:click='count--'>-</button>
+            <p>{{ count }}</p>
+            <button class='rotate' v-on:click='rotation+=90'>ROTATE ({{rotation}})</button>
         </div>
-    `
+    `,
+    methods: {
+        some: function () {
+          this.rotate(90)
+        }
+    }
 })
 
 new Vue({ el: '#vue-app' })
