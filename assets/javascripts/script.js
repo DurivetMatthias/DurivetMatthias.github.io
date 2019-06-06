1893284bd02fafdfce1661a68d0f9d3bbe3f8c4e
DurivetMatthias <matthias.durivet@student.howest.be>
2019-06-06 12:58:13 +0200

    overwrite with life counter

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,107 +1,16 @@
-const home = document.getElementById('homeScreen');
-const katakana = document.getElementById('katakanaScreen');
-const flashcard = document.getElementById('flashcardScreen');
-const stats = document.getElementById('statisticsScreen');
-const settings = document.getElementById('settingsScreen');
-//const screens = [home, katakana, flashcard, stats, settings];
-const buttonToScreen = {'katakana': katakana, 'stats': stats, 'flashcard': flashcard};
-
-//alert("v27");
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('../../worker.js', {scope: '../../'})
-            .then(function (reg) {
-                // registration worked
-                console.log('Registration succeeded. Scope is ' + reg.scope);
-            }).catch(function (error) {
-            // registration failed
-            console.log('Registration failed with ' + error);
-        });
-    }
-
-    buildStatsSection();
-
-    VOCAB_OBJECT.ROMANJI.forEach(function (letter, i) {
-        let div = document.createElement('div');
-        div.classList.add('kana');
-        if (letter !== '') div.innerHTML = letter + '<br>' + VOCAB_OBJECT.KATAKANA[i];
-        katakana.appendChild(div);
-    });
-
-    const allLi = document.getElementsByTagName('li');
-    Array.from(allLi).forEach(li => li.addEventListener('click', function () {
-        li.classList.add('buttonPush');
-        setTimeout(function () {
-            li.classList.remove('buttonPush');
-        }, 500);
-    }));
-
-    const homeLi = home.getElementsByTagName('li');
-    Array.from(homeLi).forEach(li => li.addEventListener('click', function () {
-        const screen = buttonToScreen[li.id];
-        setTimeout(function () {
-            open(screen);
-        }, 500);
-    }));
-
-    const settingsLi = settings.getElementsByTagName('li');
-    Array.from(settingsLi).forEach(li => li.addEventListener('click', function () {
-        changeStyle(li.innerHTML);
-    }));
-
-    const colorPicker = document.getElementById('colorPicker');
-    colorPicker.value = '#66CC66';
-    colorPicker.addEventListener('change', function () {
-        changeColor(colorPicker.value);
-    });
-
-    const homeIcon = document.getElementById('homeIcon');
-    homeIcon.addEventListener('click', function () {
-        open(home);
-    });
-
-    let previousScreen = home;
-    const settingsIcon = document.getElementById('settingsIcon');
-    settingsIcon.addEventListener('click', function () {
-        if (getActiveScreen() !== settings) {
-            previousScreen = getActiveScreen();
-            open(settings);
-        } else {
-            open(previousScreen);
+Vue.component('life-counter', {
+    data: function () {
+        return {
+            count: 20
         }
-    });
+    },
+    template: `
+        <div class='life-counter'>
+            <button class='add' v-on:click='count++'>+</button>
+            <p>{{ count }}</p>
+            <button class='sub' v-on:click='count--'>-</button>
+        </div>
+    `
+})
 
-    Array.from(document.getElementsByClassName('collapser')).forEach(element => element.addEventListener('click', function () {
-        element.nextElementSibling.classList.toggle('collapsed');
-    }));
-
-    function changeStyle(name) {
-        document.documentElement.style.setProperty('--baseColor', `var(--${name}Color)`, '');
-        document.documentElement.style.setProperty('--baseUrl', `var(--${name}Url)`, '');
-        colorPicker.value = getComputedStyle(document.documentElement).getPropertyValue('--baseColor').replace(' ', '');
-        /* getProperty returns an extra space at the start */
-    }
-});
-
-function changeColor(colorcode) {
-    document.documentElement.style.setProperty('--baseColor', colorcode, '');
-}
-
-function open(screen) {
-    let oldScreen = getActiveScreen();
-    oldScreen.classList.add('disappear');
-    setTimeout(function () {
-        oldScreen.classList.remove('disappear', 'active');
-        screen.classList.add('appear');
-        setTimeout(function () {
-            screen.classList.add('active');
-            screen.classList.remove('appear');
-        }, 250);
-    }, 500);
-}
-
-function getActiveScreen() {
-    return document.getElementsByClassName('active')[0];
-}
+new Vue({ el: '#vue-app' })
--- worker.js
+++ worker.js
@@ -1,53 +1,47 @@
-var CACHE = 'v27';
+var CACHE = "v1";
 
-self.addEventListener('install', function (evt) {
-    evt.waitUntil(precache());
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(precache());
 });
 
-self.addEventListener('fetch', function (evt) {
-    if (evt.request.url.toLowerCase() == "https://durivetmatthias.github.io/") {
-        evt.respondWith(fromCache("index.html"));
-    } else {
-        evt.respondWith(fromCache(evt.request));
-    }
-
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.toLowerCase() == "https://durivetmatthias.github.io/") {
+    evt.respondWith(fromCache("index.html"));
+  } else {
+    evt.respondWith(fromCache(evt.request));
+  }
 });
 
-function precache() {
-    return caches.open(CACHE).then(function (cache) {
-        return cache.addAll([
-            './index.html',
-            './worker.js',
-            './favicon.ico',
-            './assets/stylesheets/reset.css',
-            './assets/stylesheets/screen.css',
-            './assets/javascripts/flashcard.js',
-            './assets/javascripts/script.js',
-            './assets/javascripts/localforage.min.js',
-            './assets/javascripts/summary.js',
-            './assets/images/cogwheel-transparant.png',
-            './assets/images/dragon.png',
-            './assets/images/flower.png',
-            './assets/images/home-transparant.png',
-            './assets/images/origami.png',
-            './assets/images/icons/icon-72x72.png',
-            './assets/images/icons/icon-96x96.png',
-            './assets/images/icons/icon-128x128.png',
-            './assets/images/icons/icon-144x144.png',
-            './assets/images/icons/icon-152x152.png',
-            './assets/images/icons/icon-192x192.png',
-            './assets/images/icons/icon-384x384.png',
-            './assets/images/icons/icon-512x512.png',
-            './assets/manifest/manifest'
-        ]);
-    });
+async function precache() {
+  const cache = await caches.open(CACHE);
+  return cache.addAll([
+    "./index.html",
+    "./worker.js",
+    "./favicon.ico",
+    "./assets/stylesheets/reset.css",
+    "./assets/stylesheets/screen.css",
+    "./assets/javascripts/flashcard.js",
+    "./assets/javascripts/script.js",
+    "./assets/javascripts/localforage.min.js",
+    // './assets/javascripts/summary.js',
+    // './assets/images/cogwheel-transparant.png',
+    // './assets/images/dragon.png',
+    // './assets/images/flower.png',
+    // './assets/images/home-transparant.png',
+    // './assets/images/origami.png',
+    "./assets/images/icons/icon-36.png",
+    "./assets/images/icons/icon-48.png",
+    "./assets/images/icons/icon-72.png",
+    "./assets/images/icons/icon-96.png",
+    "./assets/images/icons/icon-144.png",
+    "./assets/images/icons/icon-192.png",
+    "./assets/images/icons/icon-512.png",
+    "./assets/manifest/manifest"
+  ]);
 }
 
-function fromCache(request) {
-    return caches.open(CACHE).then(function (cache) {
-        console.log(request);
-        return cache.match(request).then(function (matching) {
-            return matching || Promise.reject('no-match');
-        });
-    });
+async function fromCache(request) {
+  const cache = await caches.open(CACHE);
+  const matching = await cache.match(request);
+  return matching || Promise.reject("no-match");
 }
