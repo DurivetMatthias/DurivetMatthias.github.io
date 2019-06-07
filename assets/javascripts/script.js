641e7b55f3f49ccdcc1b2e28d961e168813aab98
DurivetMatthias <matthias.durivet@student.howest.be>
2019-06-08 00:22:26 +0200

    add style

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,28 +1,44 @@
-Vue.component('life-counter', {
-    props: ['spin'],
+Vue.component("life-counter", {
+    props: ["initRotation"],
     data: function () {
         return {
             count: 20,
-            rotation: 0,
-            image: ""
+            image: "",
+            rotation: this.initRotation,
+            isEdit: false
         }
     },
     computed:{
         computedStyle () {
-            return 'transform: rotate(' + this.spin + this.rotation + 'deg); backgroundImage: url(' + this.image + ')'
+            let rotateStyle;
+            let backgroundStyle = "backgroundImage: url(" + this.image + ")";
+            if(this.isEdit){
+                rotateStyle = "transform: rotate(" + 0 + "deg)"
+            }else{
+                rotateStyle = "transform: rotate(" + this.rotation + "deg)"
+            }
+            return rotateStyle + ";" + backgroundStyle;
         }
     },
     template: `
-        <div class='life-counter' :style=computedStyle>
-            <button class='add' v-on:click='count++'>+</button>
-            <input type=text v-on:keyup.enter=changeImage></input>
-            <button class='sub' v-on:click='count--'>-</button>
-            <p>{{ count }}</p>
-            <button class='rotate' v-on:click='rotation+=90'>ROTATE ({{rotation}})</button>
+        <div class="life-counter" :style=computedStyle>
+            <div class="display">
+                <button class="count" v-on:click="count--"><</button>
+                <p>{{ count }}</p>
+                <button class="count" v-on:click="count++">></button>
+            </div>
+            <div class="rotation">
+                <button class="rotate" v-on:click="rotation+=90">⭮</button>
+                <button class="edit" v-on:click="isEdit = !isEdit">✎</button>
+                <button class="rotate" v-on:click="rotation-=90">⭯</button>
+            </div>
+            <div class="edit-menu" v-show="isEdit">
+                <button class="apply" v-on:click="isEdit=false">Apply</button>
+            </div>
         </div>
     `,
     methods: {
-        changeImage: function (event) {
+        changeImage: (event) => {
             let search = event.srcElement.value;
             let searchUrl = "https://api.scryfall.com/cards/named?fuzzy="+search
             fetch(searchUrl)
@@ -31,9 +47,9 @@ Vue.component('life-counter', {
                 })
                 .then(cardJson => {
                     this.image = cardJson.image_uris.art_crop
-                });
+                }).catch(error => console.log(error));
         }
     }
 })
 
-new Vue({ el: '#vue-app' })
+new Vue({ el: "#vue-app" })
