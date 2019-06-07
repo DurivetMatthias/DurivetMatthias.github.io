a81d44d20ca214025a30440e2bc64d99e4b1beee
DurivetMatthias <matthias.durivet@student.howest.be>
2019-06-07 17:39:39 +0200

    variable images

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -3,25 +3,35 @@ Vue.component('life-counter', {
         return {
             count: 20,
             rotation: 0,
+            image: ""
         }
     },
     computed:{
-        rstyle () {
-            return 'transform: rotate(' + this.rotation + 'deg)'
+        computedStyle () {
+            return 'transform: rotate(' + this.rotation + 'deg); backgroundImage: url(' + this.image + ')'
         }
     },
     template: `
-        <div class='life-counter' :style="rstyle">
-            {{rstyle}}
+        <div class='life-counter' :style=computedStyle>
+            {{computedStyle}}
             <button class='add' v-on:click='count++'>+</button>
+            <input type=text v-on:keyup.enter=changeImage></input>
             <button class='sub' v-on:click='count--'>-</button>
             <p>{{ count }}</p>
             <button class='rotate' v-on:click='rotation+=90'>ROTATE ({{rotation}})</button>
         </div>
     `,
     methods: {
-        some: function () {
-          this.rotate(90)
+        changeImage: function (event) {
+            let search = event.srcElement.value;
+            let searchUrl = "https://api.scryfall.com/cards/named?fuzzy="+search
+            fetch(searchUrl)
+                .then(response => {
+                    return response.json();
+                })
+                .then(cardJson => {
+                    this.image = cardJson.image_uris.art_crop
+                });
         }
     }
 })
