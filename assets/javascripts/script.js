1d4abf4de0cd2047e94fabafde3b49ef81b7183e
DurivetMatthias <matthias.durivet@student.howest.be>
2019-07-22 09:35:16 +0200

    remove live background, update info button

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -2,31 +2,16 @@ let main = new Vue({
     el: "#vue-app",
     data: {
         apps: ["Camera App", "Dice App", "Life App"],
-        rotationEnabled: window.DeviceOrientationEvent,
-        rotationStyle: "",
+        rotationEnabled: window.DeviceOrientationEvent
     },
     methods: {
-        // Dice orientation
-        // Also flips text
-        // onOrientationChange: function () {
-        //     if (screen.orientation.angle == 180) {
-        //         this.rotationStyle = "transform: rotate(180deg) scaleX(-1)";
-        //     } else if (screen.orientation.angle == 270) {
-        //         this.rotationStyle = "transform: rotate(180deg) scaleY(-1)";
-        //     }
-        // },
     },
     mounted: function () {
         ///Live Background
-        if (window.DeviceOrientationEvent) {
+        if (this.rotationEnabled) {
             console.log("Orientation Event supported");
         } else {
             console.log("Orientation Event not supported");
         }
-        window.addEventListener("deviceorientation", function (event) {
-            let rotateX = (Math.round(event.beta)) * 2;
-            let rotateY = (Math.round(event.gamma)) * 2;
-            document.getElementById("cubeContainer").style = `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);`
-        }, true);
     }
 })
